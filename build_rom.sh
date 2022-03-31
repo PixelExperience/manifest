8300753d85742672be6fee8361f81a788e97278c
RaunakSingh55 <102671880+RaunakSingh55@users.noreply.github.com>
2022-03-31 12:37:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,3 +6,9 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # build rom
 source build/envsetup.sh
 lunch derp_rolex-user
+export TZ=Asia/Dhaka #put before last build command
+mka derp
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1
+
